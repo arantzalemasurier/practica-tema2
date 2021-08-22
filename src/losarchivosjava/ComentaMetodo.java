986a5978b153ec324c66dd448ea8157b004acd9f
arant <arant@host.docker.internal>
2021-08-22 19:04:26 +0200

    practica

---
+++ src/losarchivosjava/ComentaMetodo.java
@@ -0,0 +1,21 @@
+package losarchivosjava;
+
+/** con esta clase vamos a realizar la sumatoria de las variables a y b
+ * @author: Arantza del Barrio
+ * @since: 2021/07/08
+ * @version: 0.3
+ */
+public class ComentaMetodo {
+
+	/** en este caso como sólo usaremos numeros enteros utilizamos int, de esta forma declararemos todas las variables (a, b y c)
+ 	 *@param int a es el primer parámetro para el método sumar
+	 *@param int b es el segundo parámetro para el método sumar
+	 *@return nos devolverá la suma de las variables a y b
+	 */
+	public static int sumar(int a, int b) { 
+		int c;
+		c = a + b;
+		return c;
+		
+	}
+}
